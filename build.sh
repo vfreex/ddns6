089227fb630d11fede495568fcbf3d49a2c6a492
Yuxiang Zhu <yuxzhu@redhat.com>
2020-02-16 05:12:58 +0800

    add note for staic/dynamic linking

--- build.sh
+++ build.sh
@@ -21,15 +21,16 @@ export RUST_COMPILER_RT_ROOT=$PWD/../llvm-project/compiler-rt
 # needs to hack ~/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src/libunwind/lib.rs
 
 # link with LLD
-OBJS="-C link-arg=$SYSROOT/lib/crt1.o -C link-arg=$SYSROOT/lib/crti.o -C link-arg=$SYSROOT/lib/crtn.o"
-export RUSTFLAGS="-C target-feature=-crt-static -C linker-flavor=ld.lld -C linker=ld.lld $OBJS -C link-arg=--no-pie -C link-arg=-dynamic-linker=/lib/ld-musl-mips-sf.so.1 -L native=$SYSROOT/lib -L native=$LIBGCC_STATIC -L native=$LIBGCC_SHARED"
+# don't set OBJS on static linking
+OBJS= #"-C link-arg=$SYSROOT/lib/crt1.o -C link-arg=$SYSROOT/lib/crti.o -C link-arg=$SYSROOT/lib/crtn.o"
+export RUSTFLAGS="-C target-feature=+crt-static -C linker-flavor=ld.lld -C linker=ld.lld -C link-arg=--no-pie -C link-arg=-dynamic-linker=/lib/ld-musl-mips-sf.so.1 -L native=$SYSROOT/lib -L native=$LIBGCC_STATIC -L native=$LIBGCC_SHARED"
 RUSTFLAGS="$RUSTFLAGS -C link-arg=-znotext"
 # link with GCC
 #export RUSTFLAGS="-C target-feature=-crt-static -C linker=mips-24kc-linux-musl-ld -C link-arg=-nostdlib -C link-arg=-L$SYSROOT/lib -C link-arg=-L/Volumes/Builder/x-tools/mips-unknown-linux-musl/lib/gcc/mips-unknown-linux-musl/7.4.0 -C link-arg=-lgcc"
 #rm -rf ~/.xargo/lib/rustlib/mips-unknown-linux-musl
-#rm -rf ~/.xargo
-#cargo clean
-xargo -v build --target=$TARGET #--release
+rm -rf ~/.xargo
+cargo clean
+xargo -v rustc --target=$TARGET -- $OBJS #--release
 llvm-strip target/$TARGET/*/test-rust
 scp target/$TARGET/debug/test-rust root@172.21.0.1:
 #scp target/$TARGET/debug/test-rust yux@172.23.1.2:
